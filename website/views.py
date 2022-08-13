14fc9de97406672930a2f2023d322a97b0fa1e96
piotr <piotrekzrydek@gmial.com>
2022-08-13 22:37:06 +0200

    login page signup page and flash mess

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
-    pass
+    return render_template('home.html')
 
 
