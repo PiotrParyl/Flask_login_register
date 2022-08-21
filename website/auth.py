1b86cc54d1473e1178f68e3d7707805fc5de05e8
piotr <piotrekzrydek@gmial.com>
2022-08-21 15:50:45 +0200

    working register page, secces add user to db

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth',__name__)
 
@@ -37,7 +39,7 @@ def sign_in ():
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
-            pass
+            return redirect(url_for('views.home'))
 
 
     return render_template('sign_up.html')
