3e3b05459b458202b658b9853be448a27752cf9d
piotr <piotrekzrydek@gmial.com>
2022-08-21 16:20:02 +0200

    create home page button and place to put notes

--- website/auth.py
+++ website/auth.py
@@ -8,13 +8,33 @@ auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+ 
+    return render_template("login.html", user=current_user)
+
+
+
 
 @auth.route('/logout')
+@login_required
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
 
 @auth.route('/sign-in', methods=['GET','POST'])
 def sign_in ():
@@ -24,7 +44,10 @@ def sign_in ():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First name must be greater than 1 character.', category='error')
